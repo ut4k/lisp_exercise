aa03a8d90f41853f3f336c7729b77e8075436603
ut4k <aquarium011@gmail.com>
2019-02-09 23:53:44 +0900

    途中

--- grand_theft_wumpus.lisp
+++ grand_theft_wumpus.lisp
@@ -20,3 +20,47 @@
   (apply #'append (loop repeat *edge-num*
 		      collect (edge-pair (random-node) (random-node)))))
 
+;;loopコマンド
+
+(loop repeat 10 collect 1)
+
+(loop for n from 1 to 10 collect n)
+
+(loop for n from 1 to 10 collect (+ 100 n))
+
+;;接続されていないノードをつなぐ
+(defun direct-edges (node edge-list)
+  (remove-if-not (lambda (x)
+		   (eql (car x) node))
+		 edge-list))
+
+
+(defun get-connected (node edge-list)
+  (let ((visited nil))
+    (labels ((traverse (node)
+	       (unless (member node visited)
+		 (push node visited)
+		 (mapc (lambda (edge)
+			 (traverse (cdr edge)))
+		       (direct-edges node edge-list)))))
+      (traverse node))
+    visited))
+
+(defun find-islands (nodes edge-list)
+  (let ((islands nil))
+    (labels ((find-island (nodes)
+	       (let* ((connected (get-connected (car nodes) edge-list))
+		      (unconnected (set-difference nodes connected)))
+		 (push connected islands)
+		 (when unconnected
+		   (find-island unconnected)))))
+      (find-island nodes))
+    islands))
+
+(defun connect-widh-bridges (islands)
+  (when (cdr islands)
+    (append (edge-pair (caar islands) (caadr islands))
+	    (connect-with-bridges (cdr islands)))))
+
+(defun connect-all-islands (nodes edge-list)
+  (append (connect-width-bridges (find-islands nodes edge-list)) edge-list))
