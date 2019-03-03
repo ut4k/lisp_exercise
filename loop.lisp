a106dfcd95b7f8a804be782f2fc2d665453534f1
ut4k <aquarium011@gmail.com>
2019-03-03 18:54:38 +0900

    loopを学ぶ

---
+++ loop.lisp
@@ -0,0 +1,47 @@
+(loop for i
+   below 5
+   sum i)
+
+(loop for i
+   from 5
+   to 10
+   sum i)
+
+(loop for i
+   below 5
+   do (print i))
+
+(loop for i
+   below 10
+   when (oddp i)
+   sum i)
+
+(loop for i
+   from 0
+   do (print i)
+   when (= i 5)
+   return 'falafel)
+
+(loop for i
+   in '(2 3 4 5 6)
+   collect (* i i))
+
+(loop for x below 10
+   for y below 10
+   collect (+ x y))
+
+(loop for x below 10
+   collect (loop for  y below 10
+	      collect (+ x y)))
+
+(loop for i
+   from 0
+   for day
+   in '(monday tuesday wednesday thursday friday saturday sunday)
+   collect (cons i day))
+
+;;ループマクロで使えるコマンドはめちゃくちゃいっぱいある
+;;do, repeat, named, using, being, .................
+
+
+
