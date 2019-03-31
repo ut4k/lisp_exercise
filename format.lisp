aa803ed33ebbc038c54510caa95591afcadab8cc
ut4k <aquarium011@gmail.com>
2019-03-31 15:58:27 +0900

    整形いろいろ

--- format.lisp
+++ format.lisp
@@ -1,4 +1,4 @@
-(format t "Add onion rings for only ~$ dollars more!" 1.5)
+HHH(format t "Add onion rings for only ~$ dollars more!" 1.5)
 
 (princ (reverse
 	(format nil "Add onion rings for only ~$ dollars more!" 1.5)))
@@ -33,3 +33,47 @@
 (format t "Percentages are ~,,2f percent better than fractions" 0.77)
 
 (format t "I wish I had ~$ dollars in my bank account." 100000.2)
+
+(progn (princ 22)
+       (terpri)
+       (princ 33))
+
+(progn (princ 22)
+       (fresh-line)
+       (fresh-line)
+       (princ 33))
+
+(progn (format t "this is on one line ~%")
+       (format t "~%this is on another line"))
+
+(format t "this will print ~5%on two lines spread far apart")
+
+(defun random-animal ()
+  (nth (random 5) '("dog" "tick" "tiger" "walrus" "kangaroo")))
+
+(loop repeat 10
+   do (format t "~5t~a ~15t~a ~25t~a~%"
+	      (random-animal)
+	      (random-animal)
+	      (random-animal)))
+
+(loop repeat 10
+   do (format t "~30<~a~;~a~;~a~>~%"
+	      (random-animal)
+	      (random-animal)
+	      (random-animal)))
+
+;;中央揃え
+(loop repeat 10 do (format t "~30:@<~a~>~%" (random-animal)))
+
+(loop repeat 10
+   do (format t "~30:@<~a~;~a~;~a~>~%"
+	      (random-animal)
+	      (random-animal)
+	      (random-animal)))
+
+(loop repeat 10
+   do (format t "~10:@<~a~>~10:@<~a~>~10:@<~a~>~%"
+	      (random-animal)
+	      (random-animal)
+	      (random-animal)))
