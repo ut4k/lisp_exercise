1f31e0bf9951cf5a3d4636c06ffe3e50a5bd379a
ut4k <aquarium011@gmail.com>
2019-05-19 18:33:06 +0900

    遅延リスト用関数

--- lazy.lisp
+++ lazy.lisp
@@ -22,3 +22,56 @@
 
 (defun lazy-cdr (x)
   (cdr (force x)))
+
+(defun lazy-nil ()
+  (lazy nil))
+
+(defun lazy-null (x)
+  (not (force x)))
+
+;;通常リストを遅延リストに
+(defun make-lazy (lst)
+  (lazy (when lst
+	  (cons (car lst) (make-lazy (cdr lst))))))
+
+;;無限リスト
+(defparameter *integers*
+  (labels ((f (n)
+	     (lazy-cons n (f (1+ n)))))
+    (f 1)))
+
+;;遅延リストを通常リストに（↑の逆）
+(defun take (n lst)
+  (unless (or (zerop n) (lazy-null lst))
+    (cons (lazy-car lst) (take (1- n) (lazy-cdr lst)))))
+
+(defun take-all (lst)
+  (unless (lazy-null lst)
+    (cons (lazy-car lst) (take-all (lazy-cdr lst)))))
+
+;;遅延版map関数
+
+(defun lazy-mapcar (fun lst)
+  (lazy (unless (lazy-null lst)
+	  (cons (funcall fun (lazy-car lst))
+		(lazy-mapcar fun (lazy-cdr lst))))))
+
+(defun lazy-mapcan (fun lst)
+  (labels ((f (lst-cur)
+	     (if (lazy-null lst-cur)
+		 (force (lazy-mapcan fun (lacy-cdr lst)))
+		 (cons (lazy-car lst-cur) (lazy (f (lazy-cdr lst-cur)))))))
+    (lazy (unless (lazy-null lst)
+	    (f (funcall fun (lazy-car lst)))))))
+
+(defun lazy-find-if (fun lst)
+  (unless (lazy-null lst)
+    (let ((X (lazy-car lst)))
+      (if (funcall fun x)
+	  x
+	  (lazy-find-if fun (lazy-cdr lst))))))
+
+(defun lazy-nth (n lst)
+  (if (zerop n)
+      (lazy-car lst)
+      (lazy-nth (1- n) (lazy-cdr lst))))
